c920dc5ab91018844a60008f1164ebfd27270997
yana0200 <srimulyana422@gmail.com>
2023-10-03 19:59:41 +0700

    Upload awal project

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -2,8 +2,9 @@
 class Contoh1 extends CI_Controller
 {
     public function index()
-    {
-        echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Sri Mulyana saya tinggal di daerah Pontianak olahraga yang saya sukai adalah lari";
-    }
+{
+echo "<h1>Perkenalkan</h1>";
+echo"Nama saya Sri Mulyana saya tinggal di daerah Pontianak
+olahraga yang saya sukai adalah lari";
+}
 }
