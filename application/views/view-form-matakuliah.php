7d7b8bd21134e4769ae28615e19b481105e6b47d
yana0200 <srimulyana422@gmail.com>
2023-10-10 23:26:20 +0700

    Tugas Pertemuan 3 dan 4

--- application/views/view-form-matakuliah.php
+++ application/views/view-form-matakuliah.php
@@ -1,18 +1,18 @@
 <html>
-
 <head>
     <title>Form Input Matakuliah</title>
 </head>
-
 <body>
     <center>
-        <form action="<? base_url('matakuliah/cetak'); ?> method="post">
+        <form action="<?= base_url('matakuliah/cetak'); ?>"
+method="post">
             <table>
                 <tr>
                     <th colspan="3">
-                        Form Input Data Mata Kuliah
+                    Form Input Data Mata Kuliah
                     </th>
                 </tr>
+
                 <tr>
                     <td colspan="3">
                         <hr>
@@ -22,12 +22,18 @@
                     <th>Kode MTK</th>
                     <th>:</th>
                     <td>
+                        <input type="text" name="kode" id="kode">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Nama MTK</th>
+                    <td>:</td>
+                    <td>
                         <input type="text" name="nama" id="nama">
                     </td>
                 </tr>
                 <tr>
                     <th>SKS</th>
-                    <td>:</td>
                     <td>:</td>
                     <td>
                         <select name="sks" id="sks">
@@ -44,6 +50,8 @@
                     </td>
                 </tr>
             </table>
+        </form>
     </center>
 </body>
 </html>
+
